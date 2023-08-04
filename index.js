ef6f5a950bffc5c9dc1260753a165fb77501633c
Konrad <konradd@lab-id.com>
2023-08-04 17:00:25 +0200

    default js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 import { generateAndLoad } from './urely-serial-generator.js'
 
 generateAndLoad({
-  serial: 10,
-  serialLength: 12,
-  batchLength: 5,
-  batchName: 'test_10',
-  prefix: '',
-  suffix: '',
-  exportBatch: true,
-  additionalParameters: '{"productCategory":"eyewear","anotherParameter":5}'
 })
